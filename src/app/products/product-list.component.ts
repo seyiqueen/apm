0d73d8bb2f808992fe9e08d716c93715792482b1
Oluwaseyi Sodipo <sodiposeyi@gmail.com>
2024-02-10 02:23:07 +0000

    Building the Star Nested Component and Creating the Product Service and Dependency Injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { IProduct } from "./product";
 import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
+import { StarComponent } from "../shared/star.component";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
     standalone: true,
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css'],
-    imports: [NgIf, NgFor, FormsModule, LowerCasePipe, CurrencyPipe, ConvertToSpacesPipe]
+    imports: [NgIf, NgFor, FormsModule, LowerCasePipe, CurrencyPipe, ConvertToSpacesPipe, StarComponent]
 })
 export class ProductListComponent implements OnInit{
     pageTitle = 'Product List';
@@ -27,41 +29,25 @@ export class ProductListComponent implements OnInit{
     }
      
     filteredProducts: IProduct[] = [];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+    products: IProduct[] = [];
 
+    constructor(private productService: ProductService){}
+    
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
     }
-    
+
     toggleImage(): void{
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        this.listFilter = 'cart';
-        
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
+    onRatingClicked(message: string): void{
+        this.pageTitle = 'Product List: '+ message;
+    }
 }
