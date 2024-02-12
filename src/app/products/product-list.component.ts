8b5dcfb9368073863c99aa7fbceb3d794cbad419
Oluwaseyi Sodipo <sodiposeyi@gmail.com>
2024-02-12 03:32:14 +0000

    Retrieving Data using HTTP with Observables and Reactives Extensions

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,23 +1,27 @@
 import { CurrencyPipe, LowerCasePipe, NgFor, NgIf } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { IProduct } from "./product";
 import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
 import { StarComponent } from "../shared/star.component";
 import { ProductService } from "./product.service";
+import { HttpClientModule } from "@angular/common/http";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'pm-products',
     standalone: true,
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css'],
-    imports: [NgIf, NgFor, FormsModule, LowerCasePipe, CurrencyPipe, ConvertToSpacesPipe, StarComponent]
+    imports: [NgIf, NgFor, FormsModule, LowerCasePipe, CurrencyPipe, ConvertToSpacesPipe, StarComponent,  HttpClientModule]
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit, OnDestroy{
     pageTitle = 'Product List';
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
+    errorMessage: string = '';
+    sub!: Subscription;
     private _listFilter: string = '';
     get listFilter(): string {
         return this._listFilter;
@@ -32,7 +36,7 @@ export class ProductListComponent implements OnInit{
     products: IProduct[] = [];
 
     constructor(private productService: ProductService){}
-    
+
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy));
@@ -43,8 +47,17 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },    
+            error: err => this.errorMessage = err
+        });
+        
+    }
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
     onRatingClicked(message: string): void{
